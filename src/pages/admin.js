f485b68cbe38a750834e2791a78fadec042bc687
Dimitrysaf <demetresmeliates+github@gmail.com>
2026-05-01 10:48:43 +0300

    Fixed a css import bug for easymde

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,5 +1,4 @@
 import EasyMDE from 'easymde';
-import 'easymde/dist/easymde.min.css';
 import { updateContent } from '../components/Layout.js';
 import { navigate } from '../router.js';
 import { renderError } from '../components/ErrorPage.js';
